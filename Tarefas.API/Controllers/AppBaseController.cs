afa9524c9fd148d0637636eb3690e9a1b3a723ee
Euler Vital <eulervital@outlook.com>
2022-03-20 13:50:13 -0300

    Add project files.

---
+++ Tarefas.API/Controllers/AppBaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarefas.API.Controllers
+{
+    public class AppBaseController : ControllerBase
+    {
+        protected readonly IServiceProvider _serviceProvider;
+
+        public AppBaseController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+}
---
+++ Tarefas.API/Helps/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarefas.API.Helps
+{
+    public class DependencyInjection
+    {
+        public static void Register(IServiceCollection svc)
+        {
+            RepositoryDependences(svc);
+        }
+
+        private static void RepositoryDependences(IServiceCollection svc)
+        {
+            //Serviços da aplicação
+        }
+    }
+}
